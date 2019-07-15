466cf0f86e2d6ea82ee49b97d55cafddfe13e386
YelenaMerzlyakova <merzlyakova.lena@gmail.com>
2019-07-15 22:18:31 +0200

    fixed catch error

--- js.js
+++ js.js
@@ -8,48 +8,47 @@ let clearMoves = document.getElementById("moves");
 let pokeEvo = document.getElementById("evo");
 
 function getPokemonData() {
-  const moves = [];
-  console.log(apiUrl);
+    pokemonImage.style.display = "inline";
+    const moves = [];
 
-  axios.get(apiUrl + input.value).then(function(response) {
-    console.log(response);
-    pokemonName.innerHTML = response.data.forms[0].name + " ID: " + response.data.id;
-    var ID = response.data.id;
-    console.log(ID);
+    console.log(apiUrl + input.value);
 
-    pokemonImage.src = response.data.sprites.front_default;
-    for (let i = 0; i < 4; i++) {
-      moves.push(response.data.moves[i].move.name);
-      pokemonMoves[i].innerHTML = moves[i];
-    }
-    
-    var apiEvo = `https://pokeapi.co/api/v2/pokemon-species/${ID}`;
-    axios
-      .get(apiEvo)
-      .then(function(responseEvo) {
-        console.log(responseEvo.data);
-        //pokeEvo.innerHTML = responseEvo.data.evolves_from_species.name;
-        if (responseEvo.data.evolves_from_species == null) {
-          pokeEvo.innerHTML = "Basic Pokemon";
-        } else {
-          pokeEvo.innerHTML = responseEvo.data.evolves_from_species.name;
-        }
-        console.log(pokeEvo.innerHTML);
-        
-      })
-      .catch(function(error) {
-        pokemonName.innerHTML = "(An error has occurred.)";
-        //pokeEvo.innerHTML = "";
-        pokemonImage.src = "";
-        clearMoves.innerHTML = "";
-      });
-  });
+    axios.get(apiUrl + input.value)
+        .then(function(response) {
+            pokemonName.innerHTML = response.data.forms[0].name + " ID: " + response.data.id;
+            var ID = response.data.id;
+
+            pokemonImage.src = response.data.sprites.front_default;
+            for (let i = 0; i < 4; i++) {
+                moves.push(response.data.moves[i].move.name);
+                pokemonMoves[i].innerHTML = moves[i];
+            }
+
+            var apiEvo = `https://pokeapi.co/api/v2/pokemon-species/${ID}`;
+            axios
+                .get(apiEvo)
+                .then(function(responseEvo) {
+                    //pokeEvo.innerHTML = responseEvo.data.evolves_from_species.name;
+                    if (responseEvo.data.evolves_from_species == null) {
+                        pokeEvo.innerHTML = "Basic Pokemon";
+                    } else {
+                        pokeEvo.innerHTML = responseEvo.data.evolves_from_species.name;
+                    }
+                })
+        })
+        .catch(function(error) {
+            pokemonName.innerHTML = "(An error has occurred.)";
+            pokemonImage.src = '';
+            //pokeEvo.innerHTML = "";
+            clearMoves.innerHTML = "";
+            pokemonImage.style.display = "none";
+        });
 }
 
 var button = document.querySelector(".pokemon-button");
 button.addEventListener("click", getPokemonData);
 input.addEventListener("keypress", function(e) {
-  if (e.keyCode == 13) {
-    getPokemonData();
-  }
+    if (e.keyCode == 13) {
+        getPokemonData();
+    }
 });
