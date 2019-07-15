0488a9df917dd05d86f036e0edf48d72d913227a
“[Yelena <[merzlyakova.lena@gmail.com]>
2019-07-15 10:52:38 +0200

    add api data to html, api functional

--- js.js
+++ js.js
@@ -1,17 +1,18 @@
-const link = "https://pokeapi.co/api/v2/polemon";
-let response = await axios.get(link);
-console.log(pokedex);
 
+const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
+let input = document.querySelector(".pokemon-input");
+const pokemonName = document.querySelector(".pokemon-name");
+const pokemonImage = document.querySelector(".pokemon-image");
+const pokemonMoves = document.querySelector(".pokemon-moves");
 
-var apiUrl = "https://pokeapi.co/api/v2/pokemon/";
-var input = document.querySelector(".pokemon-input");
-var pokemonName = document.querySelector(".pokemon-name");
-var pokemonImage = document.querySelector(".pokemon-image");
 
 function getPokemonData() {
+    console.log(apiUrl);
     axios.get(apiUrl + input.value)
     .then(function (response) {
-        pokemonName.innerHTML = response.data.forms[0].name;
+        console.log(response);
+        pokemonName.innerHTML = response.data.forms[0].name + " " + response.data.id;
+        pokemonMoves.innerHTML = response.data.moves[0].move.name;
         pokemonImage.src = response.data.sprites.front_default;
     })
     .catch(function (error) {
