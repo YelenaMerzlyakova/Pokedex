6aa2604040a76d8d7df059d1e381e0b942971576
“[Yelena <[merzlyakova.lena@gmail.com]>
2019-07-15 09:58:57 +0200

    add html, css and js

--- js.js
+++ js.js
@@ -1,3 +1,24 @@
 const link = "https://pokeapi.co/api/v2/polemon";
 let response = await axios.get(link);
 console.log(pokedex);
+
+
+var apiUrl = "https://pokeapi.co/api/v2/pokemon/";
+var input = document.querySelector(".pokemon-input");
+var pokemonName = document.querySelector(".pokemon-name");
+var pokemonImage = document.querySelector(".pokemon-image");
+
+function getPokemonData() {
+    axios.get(apiUrl + input.value)
+    .then(function (response) {
+        pokemonName.innerHTML = response.data.forms[0].name;
+        pokemonImage.src = response.data.sprites.front_default;
+    })
+    .catch(function (error) {
+        pokemonName.innerHTML = "(An error has occurred.)";
+        pokemonImage.src = "";
+    });
+}
+
+var button = document.querySelector(".pokemon-button");
+button.addEventListener("click", getPokemonData);
